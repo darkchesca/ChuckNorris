f972dab2b55a1934f58ee14d1ceb0ab9c8901e7f
francesca.boe <francesca.boe@accenture.com>
2019-09-20 14:40:00 +0200

    update login screen

--- screens/Login.js
+++ screens/Login.js
@@ -3,13 +3,24 @@ import {Button, Text, View} from 'react-native';
 //import { FormLabel, FormInput} from 'react-native-elements'
 
 export default class Login extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {name: ''};
+    }
+
+
+    dispayName = (inputName) => {
+        this.setState({name: inputName});
+
+    }
     render() {
         return (
             <View>
                 <Text>Tell me your name</Text>
                 <Button
                     title="Play"
-
+                    onPress={this.displayName()}
                 />
             </View>
 
